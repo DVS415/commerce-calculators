a66589c8facd740efe40a78355d9d60b312ca8d6
Dhijuu <101403354+DVS415@users.noreply.github.com>
2025-09-21 16:35:26 +0000

    git commit -m "Implement Current Ratio calculator with button"

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
+import math
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Commerce Calculators")
+st.write("Made by DVS415")
+
+
+st.subheader("Search for calculators")
+calc = st.selectbox("Type or select", ["Home","Current ratio", "Quick Ratio", "Debt-Equity Ratio", ])
+
+if calc == "Home":
+    st.subheader("Home Page")
+    st.write("Nothing to see. Will add something here the in future.")
+elif calc == "Current ratio":
+    st.subheader("Calculating Current Ratio")
+    Current_assets = st.number_input("Enter current assets")
+    Current_liabilities = st.number_input("Enter Current liabilities")
+    
+    if st.button("Calculate current ratio"):
+        if Current_liabilities != 0:
+            quick_ratio = Current_assets / Current_liabilities * 100
+            gcd = math.gcd(int(Current_liabilities), int(Current_liabilities))
+            st.success(f"The Current ratio is {Current_assets // gcd}:{Current_liabilities // gcd}")
+            st.success(f"in %: {quick_ratio:.2f}")
+        else:
+            st.error("Current Liabilities cannot be zero!")
